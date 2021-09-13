ce198c738a8a9e355acf2b84ecffd4c99b0466a7
Noga Dayan <57255504+nogadayan@users.noreply.github.com>
2021-09-13 12:06:15 +0000

    random code :)

--- script.js
+++ script.js
@@ -1,27 +1,55 @@
-var x, y, v;
+class ball {
+  constructor(h, w, x, y, vx, vy, c) {
+  this.h = h;
+  this.w = w;
+  this.x = x;
+  this.y = y;
+  this.vx = vx;
+  this.vy = vy;
+  this.c = c;
+  }
+
+  draw(){
+    fill(this.c);
+    ellipse(this.x,this.y,50,50);
+    this.x = this.x + this.vx;
+    this.y = this.y + this.vy;
+
+    if(x > 300 || x < 0){
+    vx = vx * -1;
+    }
+    if(y > 300 || y < 0){
+    vy = vy * -1;
+    }
+  }
+}
+
+var ball1, ball2, ball3;
 
 function setup() {
-	createCanvas(500, 400);
-
-  x = 0;
-  y = 200;
-  v = 7
+	createCanvas(300, 300);
+  
+  ball1 = new Ball(50, 50, 30, 200, 5, 5, "red")
+  ball2 = new Ball(50, 50, 20, 190, 3, 3, "green")
+  ball3 = new Ball(50, 50, 10, 180, 7, 7, "yellow")
 }
 
 function draw() {
 	background(225);
   
   ellipse(x,y,50,50);
-  x = x + v
-  y = y + v
+  x = x + vx;
+  y = y + vy;
 
-  if(x > 500 || x <= 0){
-    v = v * -1;
+  if(x > 300 || x < 0){
+    vx = vx * -1;
   }
-  if(y > 500 || y <= 0){
-    v = v * -1;
+  if(y > 300 || y < 0){
+    vy = vy * -1;
   }
 
 }
+
+
 
 
